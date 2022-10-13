d68209deaf3e80b98c540f81ceb4043d18ced16b
Jakob Odersky <jakob@odersky.com>
2022-10-13 19:29:08 +0000

    Add growable connection pool

--- redicl/src/redicl/PoolApi.scala
+++ redicl/src/redicl/PoolApi.scala
@@ -25,3 +25,35 @@ trait PoolApi:
         action(client)
       finally
         clients.add(client)
+
+  /** A growable connection pool.
+    *
+    * This pool starts with an initial number of connections, and can grow if
+    * demand exceeds supply.
+    *
+    * Note: the pool does not shrink back to its initial size after demand
+    * spikes! TODO: periodically reclaim memory by removing excessive items from
+    * the pool
+    */
+  class RedisPool(
+    host: String = "localhost",
+    port: Int = 6379,
+    initialSize: Int = 8
+  ):
+    private val clients = java.util.concurrent.ConcurrentLinkedQueue[Redis]()
+
+    for _ <- 0 until initialSize do clients.add(Redis(host, port))
+
+    def withRedis[A](action: Redis => A): A =
+      var client = clients.poll()
+      if client == null then
+        client = Redis(host, port)
+
+      try
+        action(client)
+      finally
+        clients.add(client)
+
+    /** Current size of the pool. Useful for monitoring purposes. */
+    def size(): Int = clients.size()
+
